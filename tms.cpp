701df28f5dbfa769ef68cb1fb13447b6ea54336f
waliuddin1105 <waliahmed1105@gmail.com>
2024-08-11 20:09:36 +0500

    created cab class

--- tms.cpp
+++ tms.cpp
@@ -65,3 +65,25 @@ class customer{
      friend class menu;
 };
 int customer::cusId=0;
+
+template<typename i=int,typename d=double,typename s=string>
+class cabs{
+    protected:
+    i choice,kilometers;
+    d cabCost,lastCabCost;
+    s retMenu;
+    public:
+    cabs(){}
+    void cabDetails()
+	{
+        cout<<"---------------Metro Cabs---------------\n"<<endl<<endl;
+        cout<<"1. Rent a Standard Cab- Rs.30 per KM"<<endl;
+        cout<<"2. Rent a Luxury Cab- Rs.50 per KM"<<endl;
+
+        cout<<"\nTo calculate the total cost of your journey!"<<endl<<endl;
+        cout<<"--------------------------------------------------"<<endl;
+        cout<<"Enter which type of cab you wanna use:";
+        cin>>choice;
+        cout<<"How many kilometers you wanna travel:";
+        cin>>kilometers;
+        i hirecab;
