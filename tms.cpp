77a93e63ef9530459a27cb6e3b16e708ae3de20c
waliuddin1105 <waliahmed1105@gmail.com>
2024-08-11 19:45:56 +0500

    added customer class

--- tms.cpp
+++ tms.cpp
@@ -21,3 +21,44 @@ class manageMenu{
     }
     ~manageMenu(){};
 };
+
+class customer{
+    protected:
+    string name, gender, adress;
+    int age, mobile,retMenu;
+    static int cusId;
+    char all[999];
+
+    public:
+
+    customer(){}
+
+    void getDetails()
+    {
+    	system("cls");
+        ofstream out("Old_customers.txt",ios::app);// append mode 
+        { 
+            cout<<"\n\n\nEnter customer Id:";
+            cin>>cusId;
+            cout<<"Enter customer name:";
+            cin>>name;
+            cout<<"Enter age:";
+            cin>>age;
+            cout<<"Enter mobile number:";
+            cin>>mobile;
+            cout<<"Enter Adress:";
+            cin>>adress;
+            cout<<"Enter gender f/m:";
+            cin>>gender;
+            try{
+                if(gender!="f" && gender!="m")
+                throw invalid_argument("Invalid input, Gender can only be male or female");
+            }
+             catch(const invalid_argument &i)
+            {
+                cout<<i.what()<<endl<<"Please enter f/m only:";
+                cin>>gender;
+            }          
+        }
+        out<<"\n\nCustomer ID:"<<cusId<<endl<<"Customer Name:"<<name<<endl<<"Age:"<<age<<endl<<"Mobile number:"<<mobile<<endl<<"Adress:"<<adress<<endl<<"Gender:"<<gender<<endl;
+    }
